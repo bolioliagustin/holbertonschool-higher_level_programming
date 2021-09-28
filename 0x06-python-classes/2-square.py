feec25ea6f1f13a5195dd26fc11f1cf98b911e86
Agustin Bolioli <85562073+bolioliagustin@users.noreply.github.com>
2021-09-28 19:52:40 -0300

    y

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """print size and errors"""
-    def __init__(self, size = 0):
+    def __init__(self, size= 0):
 
         if type(size) is not int:
             raise TypeError("size must be an integer")
