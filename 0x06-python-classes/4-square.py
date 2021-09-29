1d0481f13fa63196620769fdf1703f46737398c3
bolioliagustin <bolioliagustin@gmail.com>
2021-09-29 11:55:51 -0700

    s

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,21 +3,21 @@
 
 
 class Square:
-    def __init__(self,size = 0):
+    def __init__(self, size=0):
         """init"""
         self.__size = size
-    
+
     def area(self):
         """defino area"""
         return (self.size ** 2)
-    
+
     @property
     def size(self):
         """devuelvo size"""
         return self.__size
-    
+
     @size.setter
-    def size (self, value):
+    def size(self, value):
         """errors"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
