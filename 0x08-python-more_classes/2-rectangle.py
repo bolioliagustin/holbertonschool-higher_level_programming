574c97d15cddc59a5fe29a6d596a5569c1000a65
boliolagustin <bolioliagustin@gmail.com>
2021-10-06 09:53:23 -0700

    s

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -44,8 +44,8 @@ class Rectangle():
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >=0")
-        self.__height = value  
-    
+        self.__height = value
+
     def area(self):
         """return area"""
         return(self.width * self.height)
