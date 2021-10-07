be53ada903b1580ca9ca9a2de6c3372d3f885c00
Agustin Bolioli <85562073+bolioliagustin@users.noreply.github.com>
2021-10-06 22:58:50 -0300

    aewa

--- 0x08-python-more_classes/7-rectangle .py
+++ 0x08-python-more_classes/7-rectangle .py
@@ -80,15 +80,3 @@ class Rectangle:
         """deletes  prints a message"""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """compare two rectangles"""
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if Rectangle.area(rect_1) >= Rectangle.area(rect_2):
-            return rect_1
-        else:
-            return rect_2
--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -80,7 +80,7 @@ class Rectangle:
         """deletes  prints a message"""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-        
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """compare two rectangles"""
