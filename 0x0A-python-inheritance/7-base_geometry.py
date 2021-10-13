307cba9f8caffc7308154509fa4b24c1dd54ead8
bolioliagustin <bolioliagustin@gmail.com>
2021-10-12 22:15:28 -0300

    s

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,4 +15,3 @@ class BaseGeometry():
             raise TypeError(f"{self.name} must be an integer")
         if value <= 0:
             raise ValueError(f"{self.name} must be grater than 0")
-        return value
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,10 +5,8 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-def Rectangle(BaseGeometry):
-    """fuction Rectangle returns errors inherits BaseGeometry"""
+class Rectangle(BaseGeometry):
+    """class Rectangle """
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        self.__width = self.integer_validator("width", width)
+        self.height = self.integer_validator("height", height)
