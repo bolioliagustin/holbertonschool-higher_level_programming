4c92632a82d4686134965cfded319a7165facfb7
bolioliagustin <bolioliagustin@gmail.com>
2021-10-22 16:38:56 -0300

    s

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -53,6 +53,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        """errors to the width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -61,6 +62,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        """errors to the height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -69,6 +71,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        """errors to the x"""
         if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -77,6 +80,7 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        """errors to the y"""
         if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
@@ -84,9 +88,11 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """define area"""
         return(self.__width * self.__height)
 
     def display(self):
+        """print rectangle"""
         if self.__width == 0 or self.__height == 0:
             print()
         for l in range(self.__y):
@@ -102,3 +108,37 @@ class Rectangle(Base):
         string = f"[{type(self).__name__}]({self.id}) \
 {self.__x}/{self.__y} - {self.__width}/{self.__height}"
         return string
+
+    def update(self, *args, **kwargs):
+        """ that argument asignad """
+        if len(args) > 0:
+            for a in range(len(args)):
+                if a == 0:
+                    self.id = args[a]
+                if a == 1:
+                    self.__width = args[a]
+                if a == 2:
+                    self.__height = args[a]
+                if a == 3:
+                    self.__x = args[a]
+                if a == 4:
+                    self.__y = args[a]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "width":
+                    self.__width = value
+                if key == "height":
+                    self.__height = value
+                if key == "x":
+                    self.__x = value
+                if key == "y":
+                    self.__y = value
+
+    def to_dictionary(self):
+        """ returns  dictionary of a Rectangle """
+        return {
+            'id': self.id, 'width': self.width,
+            'height': self.height, 'x': self.x, 'y': self.y
+        }
