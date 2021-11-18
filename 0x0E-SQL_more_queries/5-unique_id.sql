492949a63bfc975244ba10578b2e93897981a2a5
root <root@DESKTOP-1ISNH66>
2021-11-17 22:41:25 -0300

    s

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,3 @@
+-- that creates the table unique_id on your MySQL server.
+-- id INT with the default value 1 and must be unique.
+CREATE TABLE IF NOT EXISTS unique_id (id INT UNIQUE DEFAULT 1, name VARCHAR(255));
