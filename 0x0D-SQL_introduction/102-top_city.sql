95d5fcd8c25a2d54f6e91a7b005464965144fea2
root <root@DESKTOP-1ISNH66>
2021-11-18 19:57:03 -0300

    s

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,1 @@
+--  that displays the top 3 of cities temperature during July and August ordered by temperature (descending).
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,1 @@
+--  that displays the max temperature of each state (ordered by State name).
