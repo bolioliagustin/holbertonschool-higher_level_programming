49f3db995c9e6192f5f814167735a5437b9b5e98
bolioliagustin <bolioliagustin@gmail.com>
2021-12-14 13:45:02 -0300

    s

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,5 @@
+#!/bin/usr/node
+// create a Rectangle class is empty
+
+module.exports = class Rectangle {
+};
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/bin/usr/node
+// create a Rectangle class
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
