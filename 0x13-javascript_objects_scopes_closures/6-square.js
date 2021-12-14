12fd1456bb62659e4cab72e09b14ae4ccaee6c10
bolioliagustin <bolioliagustin@gmail.com>
2021-12-14 18:18:18 -0300

    fix 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -10,7 +10,7 @@ module.exports = class Square extends Rectangle {
       this.print();
     } else {
       for (let i = 0; i < this.height; i++) {
-        console.log('C'.repeat(this.width));
+        console.log(c.repeat(this.width));
       }
     }
   }
