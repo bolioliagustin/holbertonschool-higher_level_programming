048b216bb91c91365cf245b38332959abd056a85
bolioliagustin <bolioliagustin@gmail.com>
2021-12-15 20:38:32 -0300

    fix 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 array.forEach(element => console.log(element));
