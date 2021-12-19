702c64a162015d64a8535a7e6e30f76697c414c4
bolioliagustin <bolioliagustin@gmail.com>
2021-12-19 18:28:16 -0300

    fix  0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,9 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    argv = sys.argv
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" script that lists all states from the database
+hbtn_0e_0_usa and safe from SQL injections """
 
 
 if __name__ == "__main__":
@@ -9,7 +11,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
                          port=3306, user=argv[1],
                          passwd=argv[2],
-                         db=argv[3],
+                        db=argv[3],
                          charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
