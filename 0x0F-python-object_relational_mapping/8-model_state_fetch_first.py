abded90c09fe76a6a801b5b4e0c5f528faf5ed1d
bolioliagustin <bolioliagustin@gmail.com>
2021-12-19 23:31:35 -0300

    fix 7 8 9

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,13 +11,15 @@ import sys
 
 
 argv = sys.argv
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-    argv[1], argv[2], argv[3]), pool_pre_ping=True)
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-Base = declarative_base()
-first_state = Session().query(State).first()
-if first_state:
-    print("{}: {}".format(first_state.id, first_state.name))
-else:
-    print("Nothing")
+if __name__ == '__main__'':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    Base = declarative_base()
+    first_state = Session().query(State).first()
+    if first_state:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
+    Session().close()
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -11,12 +11,13 @@ import sys
 
 
 argv = sys.argv
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-    argv[1], argv[2], argv[3]), pool_pre_ping=True)
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-Base = declarative_base()
-session = Session()
-for state in session.query(State).filter(State.name.like('%a%')):
-    print("{}: {}".format(state.id, state.name))
-session.close()
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    Base = declarative_base()
+    session = Session()
+    for state in session.query(State).filter(State.name.like('%a%')):
+        print("{}: {}".format(state.id, state.name))
+    session.close()
