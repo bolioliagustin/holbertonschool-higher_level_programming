b93837c353b935ef38dcd6cdf97a7fe740b31c3a
bolioliagustin <bolioliagustin@gmail.com>
2021-12-19 23:47:40 -0300

    fix8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -4,7 +4,6 @@ from the database hbtn_0e_6_usa"""
 
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
@@ -16,11 +15,9 @@ if __name__ == '__main__':
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    Base = declarative_base()
-    Session = Session()
-    first_state = Session().query(State).first()
+    session = Session()
+    first_state = session.query(State).first()
     if first_state:
         print("{}: {}".format(first_state.id, first_state.name))
     else:
         print("Nothing")
-    Session().close()
