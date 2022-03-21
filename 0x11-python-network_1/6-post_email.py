423963fd4494271ea781ed929a64ee395ab9f41d
bolioliagustin <bolioliagustin@gmail.com>
2022-03-21 13:02:20 -0300

    d

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,11 +2,10 @@
 """takes in a URL and an email address"""
 
 
-from sys import argv
-import requests as r
-
 if __name__ == "__main__":
-    email = argv[2]
-    data = {'email': email}
-    res = r.post(argv[1], data=data.encode('utf-8'))
-    print(res.request.body)
+    from requests import post
+    from sys import argv
+
+    dict = {'email': argv[2]}
+    html = post(argv[1], data=dict)
+    print(html.text)
